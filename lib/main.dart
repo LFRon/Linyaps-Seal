6ee165212352109394ff88d69cb25b6481868cc7
mozixun <ronforever@outlook.com>
2026-01-25 12:20:07 +0800

    bump version: 0.0.8

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ void main() async {
 class MyApp extends StatefulWidget {
   
   // 在这里声明当前应用版本号
-  static String cur_version = '0.0.7';
+  static String cur_version = '0.0.8';
 
   const MyApp({super.key});
   @override
